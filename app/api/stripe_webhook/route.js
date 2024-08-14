246a6f1b447314ba408d2780ebaa657f8a3100c0
Isuu1 <jakub.babelek@gmail.com>
2024-08-14 21:18:49 +0100

    changed Stripe Webhook export method

--- app/api/stripe_webhook/route.js
+++ app/api/stripe_webhook/route.js
@@ -8,10 +8,10 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 });
 const prisma = new PrismaClient();
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
+export const runtime = "nodejs";
+
+export const api = {
+  bodyParser: false,
 };
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
