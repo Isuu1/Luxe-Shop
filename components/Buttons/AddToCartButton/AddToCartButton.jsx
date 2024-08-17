f8fa0dd6a995d3f45f5801fe13053cb6e2c814c1
Isuu1 <jakub.babelek@gmail.com>
2024-08-17 21:32:56 +0100

    Refactor addToCart button

--- components/Buttons/AddToCartButton/AddToCartButton.jsx
+++ components/Buttons/AddToCartButton/AddToCartButton.jsx
@@ -15,32 +15,35 @@ const AddToCart = ({ product }) => {
   return (
     !showCart && (
       <div className="add-to-cart">
-        <h3 className="add-to-cart__price">£{product[0].price}</h3>
-        <div>
-          <button
-            type="button"
-            className="add-to-cart__add-to-cart"
-            onClick={() => addToCart(product[0], qty)}
-          >
-            <span className="add-to-cart__qty">
-              <TbSquareRoundedMinusFilled
-                onClick={decreaseQty}
-                style={{ fontSize: "2.2rem" }}
-                className={`add-to-cart__qty__button ${
-                  qty === 1 ? "btn-not-allowed" : ""
-                }`}
-              />
-              <p className="add-to-cart__qty__value">{qty}</p>
-              <TbSquareRoundedPlusFilled
-                onClick={increaseQty}
-                style={{ fontSize: "2.2rem" }}
-                className={`add-to-cart__qty__button`}
-              />
-            </span>
-            <h3>Add to cart</h3>
-            <FaBagShopping style={{ fontSize: "1.4rem" }} />
-          </button>
+        <div className="add-to-cart__price">
+          {/* <h4>Price</h4> */}
+          <h4>£{product[0].price}</h4>
+          <div className="test"></div>
         </div>
+
+        <button
+          type="button"
+          className="add-to-cart__add-to-cart"
+          onClick={() => addToCart(product[0], qty)}
+        >
+          <span className="add-to-cart__qty">
+            <TbSquareRoundedMinusFilled
+              onClick={decreaseQty}
+              style={{ fontSize: "2rem" }}
+              className={`add-to-cart__qty__button ${
+                qty === 1 ? "btn-not-allowed" : ""
+              }`}
+            />
+            <p className="add-to-cart__qty__value">{qty}</p>
+            <TbSquareRoundedPlusFilled
+              onClick={increaseQty}
+              style={{ fontSize: "2rem" }}
+              className={`add-to-cart__qty__button`}
+            />
+          </span>
+          <h3>Add to cart</h3>
+          <FaBagShopping style={{ fontSize: "1.6rem" }} />
+        </button>
       </div>
     )
   );
