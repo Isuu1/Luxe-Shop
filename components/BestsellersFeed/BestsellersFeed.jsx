89c83bd920cf833d442b10743a024f01118e0fd1
Isuu1 <jakub.babelek@gmail.com>
2024-08-19 01:23:08 +0100

    Removed unecessary clasess

--- components/BestsellersFeed/BestsellersFeed.jsx
+++ components/BestsellersFeed/BestsellersFeed.jsx
@@ -7,12 +7,8 @@ const Bestsellers = ({ children }) => {
   const [emblaRef] = useEmblaCarousel();
 
   return (
-    <div className="home-container__bestsellers">
-      <div className="embla" ref={emblaRef}>
-        <div className="embla__container home-container__bestsellers__item">
-          {children}
-        </div>
-      </div>
+    <div className="embla" ref={emblaRef}>
+      <div className="embla__container">{children}</div>
     </div>
   );
 };
