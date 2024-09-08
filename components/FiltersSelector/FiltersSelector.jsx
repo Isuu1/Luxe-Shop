b2b10f9b4a273aec9fb0e4c177bf41671c5b01a3
Isuu1 <jakub.babelek@gmail.com>
2024-09-08 23:35:00 +0100

    Added filters styling for mobile device

--- components/FiltersSelector/FiltersSelector.jsx
+++ components/FiltersSelector/FiltersSelector.jsx
@@ -17,6 +17,10 @@ const FiltersSelector = ({ highestPrice, lowestPrice }) => {
     setCurrentMinPrice,
   } = useStateContext();
 
+  const [showFilters, setShowFilters] = useState(false);
+
+  const [windowWidth, setWindowWidth] = useState(null);
+
   //After component is rendered set prices to avoid undefined values
   useEffect(() => {
     setCurrentMaxPrice(highestPrice);
@@ -40,59 +44,116 @@ const FiltersSelector = ({ highestPrice, lowestPrice }) => {
     setCurrentMaxPrice(highestPrice);
   };
 
+  //Update window with state while window width is changed
+  useEffect(() => {
+    const handleWindowWidth = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleWindowWidth);
+    return () =>
+      window.removeEventListener("resize", handleWindowWidth);
+  }, [windowWidth]);
+
+  const mobileDevice = windowWidth < 768;
+
+  useEffect(() => {
+    if (!mobileDevice) {
+      setShowFilters(true);
+    } else {
+      setShowFilters(false);
+    }
+  }, [mobileDevice]);
+
+  console.log("Show filters? ", showFilters);
+  console.log("Is it mobile? ", mobileDevice);
+
+  const handleFilters = () => {
+    setShowFilters(!showFilters);
+  };
+
+  // useEffect(() => {
+  //   const filtersContainer = document.querySelector(
+  //     ".filters-container"
+  //   );
+  //   console.log(filtersContainer);
+  //   if (showFilters && mobileDevice) {
+  //     filtersContainer.classList.add("position-absolute");
+  //   } else {
+  //     filtersContainer.classList.remove("position-absolute");
+  //   }
+  // }, [showFilters, mobileDevice]);
+
   return (
     <div className="filters-container">
-      <p className="bold filters-container__title">Filters</p>
-      <div className="filters-container__price">
-        <p className="bold">Price</p>
-        <div className="filters-container__price__indicators">
-          <p>£{currentMinPrice}</p>
+      {!showFilters ? (
+        <button
+          className="filters-container__open-filters-button"
+          onClick={handleFilters}
+        >
+          <p>Show filters</p>
+        </button>
+      ) : (
+        <>
+          {mobileDevice && (
+            <button
+              className="filters-container__open-filters-button"
+              onClick={handleFilters}
+            >
+              <p>Hide filters</p>
+            </button>
+          )}
 
-          <p>£{currentMaxPrice}</p>
-        </div>
-        <Slider
-          allowCross={false}
-          range
-          min={lowestPrice}
-          max={highestPrice}
-          onChange={handlePriceChange}
-          value={[currentMinPrice, currentMaxPrice]}
-        />
-      </div>
-      <button
-        className="filters-container__clear-button"
-        onClick={resetPriceFilter}
-      >
-        Reset price
-      </button>
-      <div className="filters-container__rating">
-        <p className="bold">Rating</p>
-        <FiltersRatingSelector
-          value={5}
-          fullStars={5}
-          halfStars={0}
-        />
-        <FiltersRatingSelector
-          value={4}
-          fullStars={4}
-          halfStars={1}
-        />
-        <FiltersRatingSelector
-          value={3}
-          fullStars={3}
-          halfStars={2}
-        />
-        <FiltersRatingSelector
-          value={2}
-          fullStars={2}
-          halfStars={3}
-        />
-        <FiltersRatingSelector
-          value={1}
-          fullStars={1}
-          halfStars={4}
-        />
-      </div>
+          <p className="bold filters-container__title">Filters</p>
+          <div className="filters-container__price">
+            <p className="bold">Price</p>
+            <div className="filters-container__price__indicators">
+              <p>£{currentMinPrice}</p>
+
+              <p>£{currentMaxPrice}</p>
+            </div>
+            <Slider
+              allowCross={false}
+              range
+              min={lowestPrice}
+              max={highestPrice}
+              onChange={handlePriceChange}
+              value={[currentMinPrice, currentMaxPrice]}
+            />
+          </div>
+          <button
+            className="filters-container__clear-button"
+            onClick={resetPriceFilter}
+          >
+            Reset price
+          </button>
+          <div className="filters-container__rating">
+            <p className="bold">Rating</p>
+            <FiltersRatingSelector
+              value={5}
+              fullStars={5}
+              halfStars={0}
+            />
+            <FiltersRatingSelector
+              value={4}
+              fullStars={4}
+              halfStars={1}
+            />
+            <FiltersRatingSelector
+              value={3}
+              fullStars={3}
+              halfStars={2}
+            />
+            <FiltersRatingSelector
+              value={2}
+              fullStars={2}
+              halfStars={3}
+            />
+            <FiltersRatingSelector
+              value={1}
+              fullStars={1}
+              halfStars={4}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 };
