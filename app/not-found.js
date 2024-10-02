8f9262ebae5688891843f8d6394bf14b78601f47
Isuu1 <jakub.babelek@gmail.com>
2024-10-02 19:23:37 +0100

    Added not found page

--- app/not-found.js
+++ app/not-found.js
@@ -1,11 +1,14 @@
 import Link from "next/link";
+import "./not-found.scss";
 
 export default function NotFound() {
   return (
-    <div className="not-found-page">
-      <h2>Not Found</h2>
+    <div className="page not-found-page">
+      <h1>Not Found</h1>
       <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+      <Link href="/" className="not-found-page__button">
+        Return home
+      </Link>
     </div>
   );
 }
