e5e174dee5781ee24b3f60b2bf1cfcb9df67eb8b
Isuu1 <jakub.babelek@gmail.com>
2024-11-02 19:05:10 +0000

    Adjusted middleware redirect

--- middleware.js
+++ middleware.js
@@ -4,7 +4,7 @@ export default auth((req) => {
   const isLoggedIn = !!req.auth;
   console.log("ROUTE", req.nextUrl.pathname);
   console.log("Is logged in", isLoggedIn);
-  if (!req.auth && req.nextUrl.pathname === "/user") {
+  if (!req.auth && req.nextUrl.pathname.startsWith("/user")) {
     const newUrl = new URL("/auth", req.nextUrl.origin);
     return Response.redirect(newUrl);
   }
@@ -16,15 +16,3 @@ export const config = {
     "/user/:path*",
   ],
 };
-
-// export default auth((req) => {
-//   const reqUrl = new URL(req.url);
-//   if (!req.auth && reqUrl?.pathname !== "/") {
-//     return NextResponse.redirect(
-//       new URL(
-//         `/auth?callbackUrl=${encodeURIComponent(reqUrl?.pathname)}`,
-//         req.url
-//       )
-//     );
-//   }
-// });
