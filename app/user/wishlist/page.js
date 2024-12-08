573d59a7deacd0cb5254ca475d58ff1d1c114ec4
Isuu1 <jakub.babelek@gmail.com>
2024-12-08 17:26:53 +0000

    Added link to product on wishlist page

--- app/user/wishlist/page.js
+++ app/user/wishlist/page.js
@@ -3,7 +3,7 @@ import { headers } from "next/headers";
 import Image from "next/image";
 
 //Functions
-import { fetchWishlist, isMobileDevice } from "@/lib/utils";
+import getProducts, { fetchWishlist } from "@/lib/utils";
 
 //Components
 import BackButton from "@/components/Buttons/BackButton/BackButton";
@@ -18,13 +18,20 @@ import { urlFor } from "@/lib/client";
 
 //Styles
 import "./wishlist.scss";
+import Link from "next/link";
 
 export default async function page() {
   const session = await auth();
+  const products = await getProducts();
 
   const wishlistData = await fetchWishlist(session.user.id);
 
   const wishlist = wishlistData.wishlist || [];
+
+  const findLink = (id) => {
+    const product = products.find((product) => product._id === id);
+    return `/product/${product.slug.current}`;
+  };
 
   return (
     <div className="page user-page">
@@ -32,32 +39,39 @@ export default async function page() {
 
       <div className="wishlist-container">
         {wishlist.length > 0 ? (
-          wishlist.map((item) => (
-            <div className="wishlist-container__item" key={item.id}>
-              <Image
-                className="wishlist-container__item__image"
-                src={urlFor(item.productImage).toString()}
-                width={140}
-                height={140}
-                alt=""
-              />
-              <div className="wishlist-container__item__details">
-                <p className="wishlist-container__item__details__name">
-                  {item.productName}
-                </p>
-                <p className="wishlist-container__item__details__price">
-                  £{item.productPrice}
-                </p>
-                <div className="flex-center">
-                  <FaStar color="#fcf003" fontSize={"1.1rem"} />
-                  <p className="wishlist-container__item__details__stars">
-                    {item.productStars}
-                  </p>
+          wishlist.map((item) => {
+            const linkUrl = findLink(item.sanityId);
+            return (
+              <Link href={linkUrl} key={item.id}>
+                <div className="wishlist-container__item" key={item.id}>
+                  <Image
+                    className="wishlist-container__item__image"
+                    src={urlFor(item.productImage).toString()}
+                    width={140}
+                    height={140}
+                    alt=""
+                  />
+                  <div className="wishlist-container__item__details">
+                    <p className="wishlist-container__item__details__name">
+                      {item.productName}
+                    </p>
+                    <div className="flex-center-space-between">
+                      <p className="wishlist-container__item__details__price">
+                        £{item.productPrice}
+                      </p>
+                      <div className="flex-center">
+                        <FaStar color="#fcf003" fontSize={"1.1rem"} />
+                        <p className="wishlist-container__item__details__stars">
+                          {item.productStars}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <RemoveFromWishlistButton itemId={item.sanityId} />
                 </div>
-              </div>
-              <RemoveFromWishlistButton itemId={item.sanityId} />
-            </div>
-          ))
+              </Link>
+            );
+          })
         ) : (
           <h2 className="wishlist-container__wishlist-empty">
             Your wishlist is empty
